e81e28d27ee8380da2ada5231bc4a5c6d1b9d385
black143 <59355045+black-143@users.noreply.github.com>
2020-10-19 11:22:20 +0530

    Update WICP008_Minion_Chef_and_Bananas.py
    #30DaysWomenCP Challenge

--- CodeChef/WICP008_Minion_Chef_and_Bananas.py
+++ CodeChef/WICP008_Minion_Chef_and_Bananas.py
@@ -1,1 +1,25 @@
+#Problem link:https://www.codechef.com/WICP2002/problems/WICP008
 
+from sys import stdin, stdout
+from math import *
+n = stdin.readline()
+n = int(n)
+for i in range(n):
+	N,H = stdin.readline().split()
+	N = int(N);H = int(H)
+	arr = [int(c) for c in input().split()]
+	mi = 1;ma = max(arr)
+	ans = ma + 1
+	while mi<=ma:
+		mid = (mi + ma)//2
+		c = 0
+		for i in arr:
+			c += ceil(i/mid)
+		if c <= H:
+			ans = min(mid,ans)
+		if c <= H:
+			ma = mid - 1
+		else:
+			mi = mid + 1
+	stdout.write(str(ans)+'\n')
+  
