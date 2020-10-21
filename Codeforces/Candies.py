400ab9427ef07c64f9fab68ed4f0ceb1f43fb961
prahlad <47686646+Prahladk09@users.noreply.github.com>
2020-10-21 12:34:13 +0530

    Update Candies.py

--- Codeforces/Candies.py
+++ Codeforces/Candies.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+import time
+import sys
 t = int(input())
 for i in range(t):
     n = int(input())
@@ -7,3 +10,10 @@ for i in range(t):
         x=x+1
         
     print(int(n/(2**x-1)))
+ 
+
+S= range(1000)
+print(sys.getsizeof(5)*len(S))
+ 
+D= np.arange(1000)
+print(D.size*D.itemsize)
