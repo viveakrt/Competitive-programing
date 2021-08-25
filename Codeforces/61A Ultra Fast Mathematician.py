c62bf34b36e1c8c642a4c86f92a55cae531bb66d
viveakrt <vivek.y@purplle.com>
2021-08-25 10:51:42 +0530

    Modified

--- Codeforces/61A Ultra Fast Mathematician.py
+++ Codeforces/61A Ultra Fast Mathematician.py
@@ -1,10 +1,12 @@
 n=str(input())
 k=str(input())
-res=""
-for i in range(len(k)):
-    if n[i]==k[i]:
-        res+='0'
-    else:
-        res+='1'
+res = "".join('0' if n[i]==k[i] else '1' for i in range(len(k)))
 print(res)
 # https://codeforces.com/problemset/problem/61/A
+# res=""
+# for i in range(len(k)):
+#     if n[i]==k[i]:
+#         res+='0'
+#     else:
+#         res+='1'
+# print(res)
