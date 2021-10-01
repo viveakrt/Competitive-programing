d428b763b45a91bc1f34cd3104b95a7fe4f810b8
Aniket Pathak <aniketpathak028@gmail.com>
2021-10-01 15:12:16 +0530

    add problem Strange_Functions in c++

---
+++ Codeforces/1455A_Strange_Functions.cpp
@@ -0,0 +1,15 @@
+/* Question URL : https://codeforces.com/problemset/problem/1455/A*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        cout << s.length() << endl;
+    }
+}
