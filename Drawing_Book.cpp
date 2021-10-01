43028733d8172354e46030ce1de19b6aa017d9e9
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-10-01 19:16:12 +0530

    Add files via upload
    Hackerrank Algorithms solution

---
+++ Drawing_Book.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p) {
+    return min(p/2,n/2-p/2);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ Fair_Rations.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'fairRations' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY B as parameter.
+ */
+
+string fairRations(vector<int> B) {
+    int cnt=0;
+    int n = B.size();
+    for(int i=0;i<n;i++){
+        if(B[i]%2!=0&&i<n-1){
+            B[i]++;
+            B[i+1]++;
+            cnt++;
+        }
+        else if(B[i]%2!=0&&i==n-1)
+            return "NO";
+    }
+    string s = to_string(cnt*2);
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    string B_temp_temp;
+    getline(cin, B_temp_temp);
+
+    vector<string> B_temp = split(rtrim(B_temp_temp));
+
+    vector<int> B(N);
+
+    for (int i = 0; i < N; i++) {
+        int B_item = stoi(B_temp[i]);
+
+        B[i] = B_item;
+    }
+
+    string result = fairRations(B);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
