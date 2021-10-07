34002d91c47b4038c59659c46d86bf6c21efe840
Kajal Singal <48296093+kajalsingal1305@users.noreply.github.com>
2021-10-07 21:03:57 +0530

    Strange Counter

---
+++ HackerRank/Strange_Counter.py
@@ -0,0 +1,16 @@
+// Link to question: https://www.hackerrank.com/challenges/strange-code/problem
+
+// Solution 
+
+t=int(input())
+v=3
+f=3
+for i in range(1,t):
+    if(f<t):
+        i=i+v
+        v=v*2
+        f=f+v
+    else:
+        v=f+1-t
+        break
+print(v)
