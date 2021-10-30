71e935cc96e23d9ab70cef2f4cf5567232bb7e7d
johmn123-wq <iit2019088@iiita.ac.in>
2021-10-30 22:51:41 +0530

    new

---
+++ CodeChef/PROBCAT_Problem_Category.cpp
@@ -0,0 +1,24 @@
+https://github.com/johmn123-wq/Competitive-programing.gitv
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;cin>>t;
+  while(t--){
+     int x; cin>>x;
+    
+
+  if(x>=1 && x<100)
+  cout<<"Easy";
+  else if(x>=100 && x<200)
+  cout<<"Medium";
+  else if(x>=200 && x<=300)
+  cout<<"Hard";
+  
+  cout<<endl;
+  
+  }
+  
+	return 0;
+}
