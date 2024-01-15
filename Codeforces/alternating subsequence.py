27ebcedcb30afbe5930285856f107ce32405eb69
Vivek <vivek.y@purplle.com>
2024-01-15 21:56:50 +0530

    fix

--- Codeforces/alternating subsequence.py
+++ Codeforces/alternating subsequence.py
@@ -1,7 +1,7 @@
- t = int(input())
- for i in range(t):
-     n = int(input())
-     l = list(map(int,input().split()))
-     j = 0
-     k = 0
-     
+t = int(input())
+for i in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    j = 0
+    k = 0
+    
